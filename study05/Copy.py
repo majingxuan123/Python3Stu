38985ee562647bc3aaf9edb6a869627be16a8d79
majingxuan123 <jayChou163.com>
2022-05-31 17:59:49 +0800

    add class

--- study05/Copy.py
+++ study05/Copy.py
@@ -17,12 +17,12 @@ class Person:
 person1 = Person("zhoujielun", 20, Bag())
 person2 = person1
 print(id(person2) == id(person1))
-print("person1  bag",person1.bag)
-print("person2  bag",person2.bag)
+print("person1  bag",id(person1.bag))
+print("person2  bag",id(person2.bag))
 ##这个是浅拷贝  只拷贝person对象里面的值
 
 person3 = copy.copy(person1)
 # 会递归拷贝对象里面的所有子对象
 person4 = copy.deepcopy(person1)
 print(id(person3) == id(person1))
-print("person3  bag",person3.bag)
+print("person3  bag",id(person3.bag))
