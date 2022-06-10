ddfb33ae98c51c7c2e557c5b0181cfe02bbbd237
majingxuan123 <jayChou163.com>
2022-06-10 18:13:26 +0800

    增加学生项目

--- trans/ios/transIosFile.py
+++ trans/ios/transIosFile.py
@@ -1,11 +1,11 @@
-
 from trans.translate import Baidu_trans
 import os
 
 trans = Baidu_trans()
 
+
 def trans_ios(fileName):
-    f_new = open('fileName'+'.bak', 'w+', encoding='utf-8')
+    f_new = open('fileName' + '.bak', 'w+', encoding='utf-8')
     with open(fileName, 'r', encoding='utf8') as f:
         for line in f:
             print(line)
@@ -15,33 +15,30 @@ def trans_ios(fileName):
                 transStr = str(trans_trans["result"]["dst"])
                 if (transStr.find("'") != -1):
                     transStr.replace("'", " ")
-                f_new.write(transStr +'='+ split[0])
+                f_new.write(transStr + '=' + split[0])
             else:
                 f_new.write(line)
                 print()
 
-def trans_ios_tolang(fileName,toLang):
-    f_new = open('fileName'+'.bak', 'w+', encoding='utf-8')
+
+def trans_ios_tolang(fileName, toLang):
+    f_new = open(fileName + "." + toLang, 'w+', encoding='utf-8')
     with open(fileName, 'r', encoding='utf8') as f:
         for line in f:
-            print(line)
-            if ((":" in line) and ('=' in line)):
+            if ('=' in line):
                 split = line.split("=")
-                trans_trans = trans.trans_to_lang(split[1],toLang)
+                trans_trans = trans.trans_to_lang(split[1], toLang)
                 transStr = str(trans_trans["result"]["dst"])
                 if (transStr.find("'") != -1):
                     transStr.replace("'", " ")
-                f_new.write(transStr +'='+ split[0])
+                fialStr = split[0] + '=' + transStr
+                print(fialStr)
+                f_new.write(fialStr + "\n")
             else:
                 f_new.write(line)
-                print()
 
 if __name__ == '__main__':
-    files = print(os.listdir("./"))
-    for f in files:
-        if (".strings" in f):
-            # trans_ios(f)
-            trans_ios(f,"en")
 
-
-
+    list = ["cht", "en", "jp", "kor", "th", "vie", "id", "tr", "de", "fra", "pt", "fil", "ara", "may", "ru"]
+    for l in list:
+        trans_ios_tolang("Localizable.strings", l)
