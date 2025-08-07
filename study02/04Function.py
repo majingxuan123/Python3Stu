ea5ac1b4b678c0273924355839508f987022bc3d
mjx <408908536@qq.com>
2025-08-07 21:43:34 +0800

    增加函数名称调用参数

---
+++ study02/04Function.py
@@ -0,0 +1,7 @@
+
+def testFunction1(name,age,gender):
+    print(name,age,gender)
+
+
+if __name__ == "__main__":
+    testFunction1(gender="男",age=18,name="周杰伦")
--- study03/03DictStructure.py
+++ study03/03DictStructure.py
@@ -12,7 +12,6 @@ print(teacher)
 print(student)
 print("===========以下是测试===========")
 
-
 def getDic():
     ##
     # name = teacher.get("name")
@@ -23,6 +22,13 @@ def getDic():
     print("name:",name)
     print("name2:",name2)
 
+# 移除元素
+def removeDic(key):
+    teacher.pop(key)
+
+# 清空元素
+def clearDic():
+    teacher.clear()
 
 def checkDic():
     print("name" in teacher)
@@ -46,18 +52,49 @@ def testDic():
 ##字典循环
 def testDic2():
     for item in teacher:
-        print("======================")
+        print("======== 字典循环  =========")
         print("key:",item)
         print("key type :",type(item))
         print("value:",teacher.get(item))
 
 def zipDic():
-    items=["fruits","book","other"]
-    price=[5,7,9]
-    zipDic = zip(items, price)
-    ##可以转换成map或者list
-    print(type(dict(zipDic)))
-    print(type(list(zipDic)))
+    print("========== 压缩转list ============")
+    names=["周杰伦","林俊杰","邓紫棋"]
+    ids=[5,7,9]
+    zipDic = zip(names, ids)
+
+    print(f"压缩结果：{zipDic}")
+    # 这一步转换会将原始的数据改变  zipDic 就不能循环了
+    listTest =list(zipDic)
+    print(f"zip 转 list  {type(listTest)}  内容:{listTest} ")
+    newNames , newIds = zip(*listTest)
+    print(f"解包之后得到 names:{newNames} ids:{newIds}")
+    print("========== 压缩转list ============")
+
+    print("========== 压缩转 dict ============")
+    names=["周杰伦","林俊杰","邓紫棋"]
+    ids=[5,7,9]
+    zipDic = zip(names, ids)
+    print(f"压缩结果：{zipDic}")
+    # 这一步转换会将原始的数据改变  zipDic 就不能循环了
+    dictTest =dict(zipDic)
+    print(f"zip 转 dict  {type(dictTest)}  内容:{dictTest} ")
+    print(f"解包之后得到 names:{newNames} ids:{newIds}")
+
+    for name1,id1 in zipDic:
+        print(name1,id1)
+    print("========== 压缩转 dict ============")
+
+
+    print("========== 测试解包 ============")
+    names1=["周杰伦","林俊杰","邓紫棋"]
+    ids1=[5,7,9]
+    zipDic1 = zip(names1, ids1)
+    print(f"压缩结果：{zipDic1}")
+    name2,id2 = zip(*zipDic1)
+    print(f"解包结果name2:{name2} id2:{id2}")
+
+    print("========== 测试解包 ============")
 
 if __name__ == '__main__':
-    getDic()
+    testDic2()
